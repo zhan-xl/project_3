9cf2c4bd7ec25bc6f9f6c99943fc003744ee77e2
Xiaolin Zhan <zhanxl@live.com>
2023-04-17 15:42:59 -0700

    bring back username

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,36 +1,35 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import NavBar from "../components/navBar";
 import PostContainer from "../components/postContainer";
 import ProfileContainer from "../components/profileContainer";
 import axios from "axios";
+import AuthContext from "../context/AuthProvider";
 
 export default function Profile () {
-  const [userLogIn, setUserLogIn] = useState({logIn: false, userId: ""});
-  const [username, setUserName] = useState("");
 
-  useEffect(() => {
-    const userLogInTemp = JSON.parse(localStorage.getItem("userLogIn"));
-    async function fetchUserName() {
-      const res = await axios.get(userLogInTemp.userId);
-      return res.data.username;
-    }
-    if (userLogInTemp) {
-      setUserLogIn(userLogInTemp);
-      fetchUserName().then(res => {
-        setUserName(res);
-      });
-    }
-  }, []);
+  const {auth} = useContext(AuthContext);
 
-  if (!username) {
-    return null;
-  }
+
+  // useEffect(() => {
+  //   const userLogInTemp = JSON.parse(localStorage.getItem("userLogIn"));
+  //   async function fetchUserName() {
+  //     const res = await axios.get(userLogInTemp.userId);
+  //     return res.data.username;
+  //   }
+  //   if (userLogInTemp) {
+  //     setUserLogIn(userLogInTemp);
+  //     fetchUserName().then(res => {
+  //       setUserName(res);
+  //     });
+  //   }
+  // }, []);
+
 
   return (
       <div>
-        <NavBar userId={userLogIn.userId} user={username}></NavBar>
-        <ProfileContainer userId={userLogIn.userId} username={username}></ProfileContainer>
-        <PostContainer userId={userLogIn.userId} username={username}></PostContainer>
+        <NavBar user={auth.user} ></NavBar>
+        <ProfileContainer ></ProfileContainer>
+        <PostContainer ></PostContainer>
       </div>
   )
 }
