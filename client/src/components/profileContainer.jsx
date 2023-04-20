7bbef9ae7707fcd03ee26581d2096449148fbfe0
Xiaolin Zhan <zhanxl@live.com>
2023-04-19 21:50:47 -0700

    add error handling if user is no longer in the database

--- client/src/components/profileContainer.jsx
+++ client/src/components/profileContainer.jsx
@@ -7,24 +7,36 @@ export default function ProfileContainer(props) {
 
   const visitUserName = props.visitUserName;
   const [user, setUser] = useState([]);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
       if (visitUserName) {
-        const userResponse = await axios.get("/user/findByName/" + visitUserName);
-        setUser(userResponse.data); //join time, description, username
+        try {
+          const userResponse = await axios.get(
+              "/user/findByName/" + visitUserName);
+          setUser(userResponse.data); //join time, description, username
+        } catch (err) {
+          if (err.response.status === 422) {
+            setNotFound(true);
+          }
+        }
       }
     }
     fetchUser().then();
   }, [visitUserName]);
 
   return (
-      <div className="profile-container">
-        <img className="profile-avatar"
-             src={require("../img/charlie-avatar.png")} alt=""></img>
-        <div className="profile-username">{user.user}</div>
-        <div className="profile-join-time">Joined on: {new Date(user.joinTime).toLocaleString().split(",")[0]}</div>
-        <div className="description">{user.perDescr}</div>
-      </div>
+      notFound ?
+          <div>User does not exist</div>
+          :
+          (<div className="profile-container">
+            <img className="profile-avatar"
+                 src={require("../img/charlie-avatar.png")} alt=""></img>
+            <div className="profile-username">{user.user}</div>
+            <div className="profile-join-time">Joined on: {new Date(
+                user.joinTime).toLocaleString().split(",")[0]}</div>
+            <div className="description">{user.perDescr}</div>
+          </div>)
   )
 }
