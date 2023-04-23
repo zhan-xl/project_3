505b5f89e10f3f48083bacdd3d099b1aa345bf89
Xiaolin Zhan <zhanxl@live.com>
2023-04-23 00:40:04 -0700

    fixed bug

--- client/src/pages/Edit.jsx
+++ client/src/pages/Edit.jsx
@@ -2,49 +2,80 @@ import React, {useState} from "react";
 import {useLocation, useNavigate} from "react-router-dom";
 import "../style/NewPost.css"
 import axios from "axios";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
+import {
+    getStorage,
+    ref,
+    uploadBytesResumable,
+    getDownloadURL, deleteObject
+} from "firebase/storage";
 import app from '../firebase';
+import {v4 as uuid} from "uuid";
 
 export default function Edit() {
-    const [postContent, setPostContent] = useState("");
+  const [postContent, setPostContent] = useState("");
 
-    const location = useLocation();
-    const {id, path} = location.state;
-    const navigate = useNavigate();
-    const [img, setImage] = useState(null);
+  const location = useLocation();
+  const {id, path} = location.state;
+  const navigate = useNavigate();
+  const [img, setImage] = useState(null);
 
-    //updating a post by its postid
-    async function postUpdate() {
-        if (id) {
-            const storage = getStorage(app);
-            const imagePath = ref(storage, path); // getting the img path to update new img
-            const uploadTask = uploadBytesResumable(imagePath, img);
-            getDownloadURL((await uploadTask).ref).then(async (downloadURL) => {
-                await axios.put('/post/' + id, JSON.stringify({
-                    postContent, imagePath: imagePath.fullPath, 
-                    url: downloadURL,
+  //updating a post by its postid
+  async function postUpdate() {
+    if (id) {
+      const storage = getStorage(app);
+      if (img) { // there is a new image to upload
+        const imagePath = ref(storage,
+            path ? path : `/images/${img.name + uuid()}`); // getting the img path to update new img
+        const uploadTask = uploadBytesResumable(imagePath, img);
+        getDownloadURL((await uploadTask).ref).then(async (downloadURL) => {
+          await axios.put('/post/' + id, JSON.stringify({
+                postContent, imagePath: imagePath.fullPath,
+                url: downloadURL,
+              }),
+              {
+                headers: {"Content-Type": "application/json"},
+                withCredentials: true
+              }).then(navigate('/'));
+        })
+      } else if (path) { // there is no image to upload, but there is an old image to delete
+          const desertRef = ref(storage, path);
+          deleteObject(desertRef).then(async () => {
+              await axios.put('/post/' + id, JSON.stringify({
+                      postContent, imagePath: "",
+                      url: "",
                   }),
                   {
-                    headers: {"Content-Type": "application/json"},
-                    withCredentials: true
+                      headers: {"Content-Type": "application/json"},
+                      withCredentials: true
                   }).then(navigate('/'));
-            })
-        }
-    }
+          })
+      } else { // no old or new image
+          await axios.put('/post/' + id, JSON.stringify({
+                  postContent, imagePath: "",
+                  url: "",
+              }),
+              {
+                  headers: {"Content-Type": "application/json"},
+                  withCredentials: true
+              }).then(navigate('/'));
+      }
 
-    return (
-        <>
+    }
+  }
+
+  return (
+      <>
         <div className="container">
-            <div className="title">Edit post</div>
-                <textarea type="textarea" placeholder="Say something..."
+          <div className="title">Edit post</div>
+          <textarea type="textarea" placeholder="Say something..."
                     cols="32" rows="5"
-                  onChange={e => setPostContent(e.target.value)}
-                  className="textarea"/>
-                <input type="file" onChange={e => setImage(e.target.files[0])}
-                accept="image/*" >
-                </input> 
-            <div className="post-button" onClick={postUpdate}>Update</div>
-      </div>
-        </>
-    )
+                    onChange={e => setPostContent(e.target.value)}
+                    className="textarea"/>
+          <input type="file" onChange={e => setImage(e.target.files[0])}
+                 accept="image/*">
+          </input>
+          <div className="post-button" onClick={postUpdate}>Update</div>
+        </div>
+      </>
+  )
 }
